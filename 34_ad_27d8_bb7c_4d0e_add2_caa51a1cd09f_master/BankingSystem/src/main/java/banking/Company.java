5bfeb92a0b3b4247c52e96ffb3e283cb6e71ae7d
nttd <uttam.wakchaure@gmail.com>
2021-09-06 23:43:17 +0530

    Add files via upload
    Changes Done

---
+++ 34_ad_27d8_bb7c_4d0e_add2_caa51a1cd09f_master/BankingSystem/src/main/java/banking/Company.java
@@ -0,0 +1,14 @@
+package banking;
+
+public class Company implements AccountHolder {
+	private String companyName;
+
+	public Company(String companyName, int taxId) {
+		// complete the function
+	}
+
+	public String getCompanyName() {
+		// complete the function
+        return null;
+	}
+}
---
+++ 34_ad_27d8_bb7c_4d0e_add2_caa51a1cd09f_master/BankingSystem/src/main/java/banking/ConsumerAccount.java
@@ -0,0 +1,8 @@
+package banking;
+
+public class ConsumerAccount implements Account {
+	public ConsumerAccount(Person person, Long accountNumber, int pin, double currentBalance) {
+		// complete the function
+	}
+	
+}
